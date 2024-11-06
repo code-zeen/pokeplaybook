1b50b99274cc56eaf3c6ad8e1d569e7e5c1c2cc6
code-zeen <aejin.km@gmail.com>
2024-11-06 14:29:51 +0900

    style: add styling to main app

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,19 @@ function App() {
   return (
     <div>
       {questions.map(q => (
-        <div className="flex flex-col gap-1">
-          <h5>{q.question}</h5>
-          {q.options.map(o => (
-            <div className="flex gap-1">
-              <input key={o.id} type="radio" name="radio" id={`${q.id}-${o.id}`} value={o.option} />
-              <label htmlFor={`${q.id}-${o.id}`}>{o.option}</label>
-            </div>
-          ))}
+        <div className="flex flex-col gap-1 p-4">
+          <div className="flex items-center gap-4">
+            <b className="px-3.5 py-2 bg-slate-500 text-white rounded-lg">{q.id}번</b>
+            <h5>{q.question}</h5>
+          </div>
+          <div className="flex gap-8 flex-wrap">
+            {q.options.map(o => (
+              <div className="flex gap-1">
+                <input key={o.id} type="radio" name={String(q.id)} id={`${q.id}-${o.id}`} value={o.option} />
+                <label htmlFor={`${q.id}-${o.id}`}>{o.option}</label>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
