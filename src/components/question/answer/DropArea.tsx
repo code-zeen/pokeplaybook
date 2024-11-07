6671b71d98d1aea1cd7060ff29f11fe9efc8ed5f
code-zeen <aejin.km@gmail.com>
2024-11-07 10:56:21 +0900

    compose DropArea with useDrop

--- src/components/question/answer/DropArea.tsx
+++ src/components/question/answer/DropArea.tsx
@@ -1,6 +1,19 @@
+import { useDrop } from 'react-dnd'
+
+
 function DropArea() {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: 'any',
+    drop: (item: { id: number, value: string }) => {
+
+    },
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+    })
+  }))
   return (
-    <div className="flex border border-gray-300 min-h-12 rounded hover:border-sky-600" />
+    <div ref={drop} className={`flex border border-gray-300 min-h-12 rounded hover:border-sky-600 ${isOver && 'border-sky-600'}`}>
+    </div>
   )
 }
 
