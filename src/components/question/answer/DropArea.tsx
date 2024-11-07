ce48748b31e39419833b22aa808ddf3623cc992d
code-zeen <aejin.km@gmail.com>
2024-11-07 11:29:46 +0900

    apply function for dropping items

--- src/components/question/answer/DropArea.tsx
+++ src/components/question/answer/DropArea.tsx
@@ -1,16 +1,19 @@
 import { useDrop } from 'react-dnd'
+import useDragAndDrop from './useDragAndDrop.ts'
 
 
 function DropArea() {
   const [{ isOver }, drop] = useDrop(() => ({
     accept: 'any',
     drop: (item: { id: number, value: string }) => {
-
+      addDroppedItemId(item.id)
     },
     collect: monitor => ({
       isOver: monitor.isOver(),
     })
   }))
+  const { droppedItemIds, addDroppedItemId } = useDragAndDrop()
+
   return (
     <div ref={drop} className={`flex border border-gray-300 min-h-12 rounded hover:border-sky-600 ${isOver && 'border-sky-600'}`}>
     </div>
