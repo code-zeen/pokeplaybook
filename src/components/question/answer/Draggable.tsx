f9966d91607b70ca5c9cdd0abd86478c929f5400
code-zeen <aejin.km@gmail.com>
2024-11-07 11:58:12 +0900

    refactor copypasta data to mapped array

--- src/components/question/answer/Draggable.tsx
+++ src/components/question/answer/Draggable.tsx
@@ -2,21 +2,18 @@ import DraggableItem from './DraggableItem.tsx'
 import TextBox from './TextBox.tsx'
 
 function Draggable() {
+  const dragTexts = ['hello', 'world', 'test']
   return (
     <div className="flex relative border-2 border-dashed border-gray-300 rounded-xl gap-2 p-8 pt-8 mt-4">
       <div
         className="flex absolute bg-gray-100 px-4 py-1.5 rounded-3xl right-1/2 top-0 transform translate-x-1/2 -translate-y-1/2">
         <b>끌어놓으세요</b>
       </div>
-      <DraggableItem>
-        <TextBox text="hello" />
-      </DraggableItem>
-      <DraggableItem>
-        <TextBox text="world" />
-      </DraggableItem>
-      <DraggableItem>
-        <TextBox text="test" />
-      </DraggableItem>
+      {dragTexts.map(text => (
+        <DraggableItem>
+          <TextBox key={text} text={text} />
+        </DraggableItem>
+      ))}
     </div>
   )
 }
