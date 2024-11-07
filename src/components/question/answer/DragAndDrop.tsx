40881038adeff2fd2623d3d04675c2a5a075238b
code-zeen <aejin.km@gmail.com>
2024-11-07 09:43:34 +0900

    create DropArea

--- src/components/question/answer/DragAndDrop.tsx
+++ src/components/question/answer/DragAndDrop.tsx
@@ -1,9 +1,11 @@
 import Draggable from './Draggable.tsx'
+import DropArea from './DropArea.tsx'
 
 function DragAndDrop() {
   return (
     <div className="flex flex-col">
       <Draggable />
+      <DropArea />
     </div>
   )
 }
---
+++ src/components/question/answer/DropArea.tsx
@@ -0,0 +1,7 @@
+function DropArea() {
+  return (
+    <div className="flex border border-gray-300 min-h-12 rounded hover:border-sky-600" />
+  )
+}
+
+export default DropArea
