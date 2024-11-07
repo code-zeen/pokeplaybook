8e0e3d1b8e72e5ffdb4aae90e3fe614ef1813910
code-zeen <aejin.km@gmail.com>
2024-11-07 15:04:27 +0900

    adjust reponseive css

--- src/components/question/QuestionTitle.tsx
+++ src/components/question/QuestionTitle.tsx
@@ -7,7 +7,7 @@ interface QuestionTitleProps {
 
 function QuestionTitle({ id, title }: QuestionTitleProps) {
   return (
-    <div className="flex items-center gap-4 bg-slate-100 p-2">
+    <div className="flex items-center gap-4 bg-slate-100 p-2 md:p-4">
       <b className="px-3 py-2 bg-slate-500 text-white rounded-lg whitespace-nowrap">{id}번</b>
       <h5>{title}</h5>
     </div>
