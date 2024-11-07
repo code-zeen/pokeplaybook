08b2a4412dfeda9b66e5d0b663af0fcef53bf2a3
code-zeen <aejin.km@gmail.com>
2024-11-07 09:33:01 +0900

    split drag portion into smaller components

--- src/components/question/answer/DragAndDrop.tsx
+++ src/components/question/answer/DragAndDrop.tsx
@@ -1,14 +1,9 @@
-import DraggableItem from './DraggableItem.tsx'
+import Draggable from './Draggable.tsx'
 
 function DragAndDrop() {
   return (
     <div className="flex">
-      <div className="flex relative border-2 border-dashed border-gray-300 rounded-xl gap-2 p-8 pt-8 mt-4">
-        <div className="flex absolute bg-gray-200 px-4 py-1.5 rounded-3xl right-1/2 top-0 transform translate-x-1/2 -translate-y-1/2"><b>끌어놓으세요</b></div>
-        <DraggableItem>test</DraggableItem>
-        <DraggableItem>hello</DraggableItem>
-        <DraggableItem>world</DraggableItem>
-      </div>
+      <Draggable />
     </div>
   )
 }
