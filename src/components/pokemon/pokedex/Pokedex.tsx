a4578bfad08788d4d4a8075b48f92f67b4f34bd1
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-16 00:27:11 +0900

    componentize gray pokeball

--- src/components/pokemon/pokedex/Pokedex.tsx
+++ src/components/pokemon/pokedex/Pokedex.tsx
@@ -3,6 +3,7 @@ import PokedexHeader from "./PokedexHeader";
 import PokedexList from "./PokedexList";
 import PokemonName from "./PokemonName";
 import ContainerWithSideBorder from "./ContainerWithSideBorder";
+import PokeballGrayBg from "./PokeballGrayBg";
 
 interface PokedexProps {
     pokemons: PokemonType[]
@@ -17,20 +18,8 @@ function Pokedex({ pokemons }: PokedexProps) {
                     <div className="flex flex-col justify-between">
                         <PokemonName />
 
-                        <div className="flex flex-col justify-center items-center">
-                            <div
-                                className="w-48 h-24 border-8 border-gray-100 bg-gray-300 rounded-t-full transform"
-                                style={{ transform: 'translateY(1px)' }}>
-                            </div>
-                            <div
-                                className="w-48 h-24 border-8 border-gray-100 bg-gray-200 rounded-b-full transform"
-                                style={{ transform: 'translateY(-1px)' }}>
-                            </div>
-                            <div className="bg-gray-100 w-24 h-24 rounded-full absolute"></div>
-                            <div className="bg-gray-200 w-16 h-16 rounded-full absolute"></div>
-                        </div>
-
-
+                        <PokeballGrayBg />
+                        
                         <ContainerWithSideBorder>
                             <p>seen: 11</p>
                             <p>owned: 2</p>
