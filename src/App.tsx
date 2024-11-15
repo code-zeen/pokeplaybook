2ea1707faa0769fd7c3f6e005e00381c1750fdbf
code-zeen <aejin.km@gmail.com>
2024-11-15 16:36:26 +0900

    fetch & render pokemon & ability

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-import { QuestionType } from './components/question/questionType.ts'
-import Question from './components/question/Question.tsx'
+import type { QuestionType } from './components/question/questionType.ts'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import PokemonCard from './components/pokemon/PokemonCard.tsx'
@@ -20,14 +19,16 @@ function App() {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="max-w-5xl mx-auto">
-        <div className="flex flex-col p-1 border gap-2">
-          {questions.map(q => (
-            <Question key={q.id} question={q} />
-          ))}
-        </div>
+      {/*<div className="max-w-5xl mx-auto">*/}
+      {/*  <div className="flex flex-col p-1 border gap-2">*/}
+      {/*    {questions.map(q => (*/}
+      {/*      <Question key={q.id} question={q} />*/}
+      {/*    ))}*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      <div className="flex flex-col items-center border">
+        <PokemonCard />
       </div>
-      <PokemonCard />
     </DndProvider>
   )
 }
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
+import type { AbilityType, PokemonType } from './pokemonType.ts'
 
 function PokemonCard() {
-  const [pokemon, setPokemon] = useState()
+  const [pokemon, setPokemon] = useState<PokemonType>()
+  const [ability, setAbility] = useState<AbilityType>()
   useEffect(() => {
     (async () => {
       await fetchPokemonByName('ditto')
@@ -9,16 +11,37 @@ function PokemonCard() {
   }, [])
 
   const fetchPokemonByName = async (name: string) => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    const data = await res.json()
+    const resPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    const dataPokemon = await resPokemon.json()
 
-    setPokemon(data)
+    const resAbility = await fetch(`https://pokeapi.co/api/v2/ability/${dataPokemon?.abilities[0].ability.name}`)
+    const dataAbility = await resAbility.json()
+
+    setPokemon(dataPokemon)
+    setAbility(dataAbility)
   }
 
+  if (!pokemon || !ability) return null
 
   return (
-    <div>
-      {pokemon?.name}
+    <div className="p-2 bg-yellow-200 w-72 border rounded">
+      <div className="flex flex-col bg-gray-100 rounded-lg">
+        <div className="flex justify-between">
+        <span>{pokemon.name}</span>
+        <span>HP {pokemon.stats.find(each => each.stat.name === 'hp')?.base_stat}</span>
+        </div>
+        <div className="flex justify-center border">
+          <img src={pokemon.sprites.front_default} alt={pokemon?.name} />
+        </div>
+        <div className="flex flex-col">
+          <div className="flex">
+            <span>Ability</span>
+            <span>{ability.name}</span>
+          </div>
+
+          {ability.flavor_text_entries.find(each => each.language.name === 'en')?.flavor_text}
+        </div>
+      </div>
     </div>
   )
 }
