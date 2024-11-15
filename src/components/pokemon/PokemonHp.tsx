7e910b2a98c486ab0f273c5f4ffaf9146dddc977
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-15 20:40:51 +0900

    adjust css

--- src/components/pokemon/PokemonHp.tsx
+++ src/components/pokemon/PokemonHp.tsx
@@ -8,7 +8,7 @@ interface PokemonHpProps {
 
 function PokemonHp({ hp, type }: PokemonHpProps) {
     return (
-        <div className="flex items-baseline gap-0.5">
+        <div className="flex items-center gap-0.5">
             <span className="text-xs">HP</span>
             <span><b>{hp}</b></span>
             <TypeIcon type={type} />
