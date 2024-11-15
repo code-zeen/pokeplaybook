26d8a11b1fd04b030bb85bb492ff3aae890f6ea2
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-16 00:25:51 +0900

    create background pokeball

--- src/components/pokemon/pokedex/Pokedex.tsx
+++ src/components/pokemon/pokedex/Pokedex.tsx
@@ -11,11 +11,30 @@ interface PokedexProps {
 function Pokedex({ pokemons }: PokedexProps) {
     return (
         <div className="h-96">
-            <div className="flex flex-col bg-gray-300">
+            <div className="flex flex-col bg-gray-100">
                 <PokedexHeader />
                 <div className="flex p-2 gap-2">
                     <div className="flex flex-col justify-between">
                         <PokemonName />
+
+                        <div className="flex flex-col justify-center items-center">
+                            <div
+                                className="w-48 h-24 border-8 border-gray-100 bg-gray-300 rounded-t-full transform"
+                                style={{ transform: 'translateY(1px)' }}>
+                            </div>
+                            <div
+                                className="w-48 h-24 border-8 border-gray-100 bg-gray-200 rounded-b-full transform"
+                                style={{ transform: 'translateY(-1px)' }}>
+                            </div>
+                            <div className="bg-gray-100 w-24 h-24 rounded-full absolute"></div>
+                            <div className="bg-gray-200 w-16 h-16 rounded-full absolute"></div>
+                        </div>
+
+
+                        <ContainerWithSideBorder>
+                            <p>seen: 11</p>
+                            <p>owned: 2</p>
+                        </ContainerWithSideBorder>
                     </div>
                     <PokedexList pokemons={pokemons} />
 
