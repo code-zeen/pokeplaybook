829888f18dd1880b41b243fc581c979fe5c3f454
aejin <aejin.km@gmail.com>
2024-11-15 19:26:04 +0900

    add PokemonMove component

---
+++ src/components/pokemon/PokemonMove.tsx
@@ -0,0 +1,12 @@
+function PokemonMove({ move }) {
+    return (
+        <div className="flex justify-between">
+            <div className="flex gap-3">
+                <span>splup</span>
+            </div>
+            <span>10</span>
+        </div>
+    )
+}
+
+export default PokemonMove
