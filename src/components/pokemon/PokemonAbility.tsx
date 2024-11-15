4bf5403eb9717bb72e5a0f92b1c9b351121b8e12
code-zeen <aejin.km@gmail.com>
2024-11-15 17:25:45 +0900

    adjust css

--- src/components/pokemon/PokemonAbility.tsx
+++ src/components/pokemon/PokemonAbility.tsx
@@ -7,10 +7,15 @@ function PokemonAbility({ name, flavorText }: PokemonAbilityProps) {
   return (
     <div className="flex flex-col">
       <div className="flex gap-1">
-        <div className=" border-2 border-gray-200 bg-rose-500 rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg pl-2 pr-4 text-white text-sm">Ability</div>
+        <div
+          className="border border-gray-400 rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg px-0.5">
+          <div className="bg-rose-500 rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg pl-2 pr-4">
+            <b className="text-xs  text-white italic">Ability</b>
+          </div>
+        </div>
         <span className="capitalize text-rose-500"><b>{name}</b></span>
       </div>
-      <p className="text-sm">{flavorText}</p>
+      <p className="text-xs">{flavorText}</p>
     </div>
   )
 }
