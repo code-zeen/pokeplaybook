a3931bdce6b6f4db7a45aaeba4edbac020b84365
code-zeen <aejin.km@gmail.com>
2024-11-15 16:51:11 +0900

    create pokemon enum

---
+++ src/enum/pokemonEnum.ts
@@ -0,0 +1,8 @@
+export enum StatNameEnum {
+  HP = 'hp',
+  ATTACK = 'attack',
+  DEFENSE = 'defense',
+  SPECIAL_ATTACK = 'special_attack',
+  SPECIAL_DEFENSE = 'special_defense',
+  SPEED = 'speed',
+}
