28900a0bb82d914552ac0d687b08b8d8110fea30
code-zeen <aejin.km@gmail.com>
2024-11-15 14:45:29 +0900

    add Select type answer component

--- src/components/question/answer/Answers.tsx
+++ src/components/question/answer/Answers.tsx
@@ -3,6 +3,7 @@ import MultipleChoice from './MultipleChoice.tsx'
 import { AnswerTypeEnum } from '../../../types/enum.ts'
 import DragAndDrop from './DragAndDrop.tsx'
 import TextInput from './TextInput.tsx'
+import Select from './Select.tsx'
 
 interface AnswerProps {
   qId: QuestionType['id']
@@ -14,12 +15,14 @@ function Answers({ qId, answerConfig, options }: AnswerProps) {
   const { type } = answerConfig
   const isMultipleChoice = type === AnswerTypeEnum.RADIO || type === AnswerTypeEnum.CHECKBOX
   const isText = type === AnswerTypeEnum.TEXT
+  const isSelect = type === AnswerTypeEnum.SELECT
   const isDragAndDrop = type === AnswerTypeEnum.DRAGNDROP
 
   return (
     <div className="flex justify-center p-8">
       {isMultipleChoice && <MultipleChoice options={options} qId={qId} type={answerConfig.type} />}
       {isText && <TextInput />}
+      {isSelect && <Select options={options} />}
       {isDragAndDrop && <DragAndDrop options={options} />}
     </div>
   )
