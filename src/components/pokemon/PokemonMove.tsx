4015972d0d5acd389c6efcd966c32c6e9f548799
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-15 21:03:03 +0900

    adjust css

--- src/components/pokemon/PokemonMove.tsx
+++ src/components/pokemon/PokemonMove.tsx
@@ -1,17 +1,24 @@
+import TypeIcon from "./TypeIcon";
+import { TypeEnum } from "../../enum/pokemonEnum";
+
 interface PokemonMoveProps {
     name: string
     power: number | null
+    type: TypeEnum
     flavorText: string
 }
 
-function PokemonMove({ name, power, flavorText }: PokemonMoveProps) {
+function PokemonMove({ name, power, type, flavorText }: PokemonMoveProps) {
     return (
         <div className="flex flex-col">
-            <div className="flex justify-between">
-                <div className="flex gap-3">
-                    <span className="capitalize"><b>{name}</b></span>
+            <div className="flex items-center">
+                <div className="flex w-1/4">
+                    <TypeIcon type={type} />
                 </div>
-                <span>{power}</span>
+                <div className="flex w-3/4 justify-between">
+                    <span className="capitalize"><b>{name}</b></span>
+                    <span><b>{power}</b></span>
+                </div>
             </div>
             <p className="text-xs">{flavorText}</p>
         </div>
