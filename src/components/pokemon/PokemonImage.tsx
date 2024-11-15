ace4e53f7837a2674e75082b43eef8680cf48ac1
aejin <aejin.km@gmail.com>
2024-11-15 19:12:07 +0900

    adjust css

--- src/components/pokemon/PokemonImage.tsx
+++ src/components/pokemon/PokemonImage.tsx
@@ -5,7 +5,7 @@ interface PokemonImageProps {
 
 function PokemonImage({ src, name }: PokemonImageProps) {
   return (
-    <div className="flex justify-center border">
+    <div className="flex justify-center mx-2 border border-gray-300 border-b-transparent">
       <img src={src} alt={name} />
     </div>
   )
