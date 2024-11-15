0700dabfccc072e0040f7db93fef0c5fcc4c3286
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-15 19:44:10 +0900

    apply PokemonMove component

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react'
-import type { AbilityType, PokemonType, StatType } from './pokemonType.ts'
+import type { AbilityType, MoveType, PokemonType, StatType } from './pokemonType.ts'
 import PokemonName from './PokemonName.tsx'
 import PokemonHp from './PokemonHp.tsx'
 import { StatNameEnum } from '../../enum/pokemonEnum.ts'
 import PokemonAbility from './PokemonAbility.tsx'
 import PokemonImage from './PokemonImage.tsx'
 import PokemonPhysicalInfo from "./PokemonPhysicalInfo.tsx";
+import PokemonMove from "./PokemonMove.tsx";
 
 function PokemonCard() {
   const [pokemon, setPokemon] = useState<PokemonType>()
   const [ability, setAbility] = useState<AbilityType>()
+  const [move, setMove] = useState<MoveType>()
   useEffect(() => {
     (async () => {
       await fetchPokemonByName('ditto')
@@ -23,26 +25,37 @@ function PokemonCard() {
     const resAbility = await fetch(`https://pokeapi.co/api/v2/ability/${dataPokemon?.abilities[0].ability.name}`)
     const dataAbility = await resAbility.json()
 
+    const resMove = await fetch(`https://pokeapi.co/api/v2/move/${dataPokemon?.moves[0].move.name}`)
+    const dataMove = await resMove.json()
+
     setPokemon(dataPokemon)
     setAbility(dataAbility)
+    setMove(dataMove)
   }
 
   const findStat = (stats: StatType[], name: StatNameEnum): StatType =>  {
     return stats.find(each => each.stat.name === name)!
   }
 
-  if (!pokemon || !ability) return null
+  const findEnglishFlavorText = (flavorTextEntries) => {
+    return flavorTextEntries.find(each => each.language.name === 'en')!
+  }
+
+  if (!pokemon || !ability || !move) return null
 
   return (
-    <div className="p-2 bg-yellow-200 w-72 border rounded">
-      <div className="flex flex-col bg-gray-100 rounded-lg">
+    <div className="p-2 bg-yellow-200 w-72 h-96 border rounded">
+      <div className="flex flex-col bg-gray-100 h-full rounded-lg">
         <div className="flex justify-between">
           <PokemonName name={pokemon.name} />
           <PokemonHp hp={findStat(pokemon.stats, StatNameEnum.HP).base_stat} />
         </div>
         <PokemonImage src={pokemon.sprites.front_default ?? ''} name={pokemon.name} />
         <PokemonPhysicalInfo number={pokemon.id} height={pokemon.height} weight={pokemon.weight} />
-        <PokemonAbility name={ability.name} flavorText={ability.flavor_text_entries.find(each => each.language.name === 'en')?.flavor_text} />
+        <div className="flex flex-col justify-around h-full p-2">
+          <PokemonAbility name={ability.name} flavorText={findEnglishFlavorText(ability.flavor_text_entries).flavor_text} />
+          <PokemonMove name={move.name} power={move.power} flavorText={findEnglishFlavorText(move.flavor_text_entries).flavor_text} />
+        </div>
       </div>
     </div>
   )
