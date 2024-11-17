d7d5fb87e5e7944805ccaef568a5f31ca16a6d77
code-zeen <aejin.km@gmail.com>
2024-11-18 08:43:21 +0900

    install pokemon-sprites

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,4 +1,8 @@
 declare module '*.png' {
-    const value: string;
-    export default value;
+  const value: string
+  export default value
+}
+declare module '@pokemon-sprites/sprites/*' {
+  const value: string
+  export default value
 }
