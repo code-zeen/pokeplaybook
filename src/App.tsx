40ab3dbdc5283be0db669a144fd1716de7e48594
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-17 13:09:28 +0900

    move generations into own file & add "special" entry

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PokemonType } from "./components/pokemon/types/pokemonType";
 import Pokedex from "./components/pokemon/pokedex/Pokedex";
 import { AbilityType } from "./components/pokemon/types/abilityType";
 import { MoveType } from "./components/pokemon/types/moveType";
+import { generations } from "./components/pokemon/pokedex/generationQuery";
 
 export interface PokedexInfo extends PokemonType {
     seen: number
@@ -34,18 +35,6 @@ function App() {
             })()
         }
     }, [ selectedPokemonId ])
-
-    const generations = [
-        { name: 1, offset: 0, limit: 151 },
-        { name: 2, offset: 151, limit: 100 },
-        { name: 3, offset: 251, limit: 135 },
-        { name: 4, offset: 386, limit: 107 },
-        { name: 5, offset: 493, limit: 156 },
-        { name: 6, offset: 649, limit: 72 },
-        { name: 7, offset: 721, limit: 88 },
-        { name: 8, offset: 809, limit: 96 },
-        { name: 9, offset: 905, limit: 120 },
-    ]
 
 
     const fetchAllPokemons = async () => {
---
+++ src/components/pokemon/pokedex/generationQuery.ts
@@ -0,0 +1,12 @@
+export const generations = [
+    { name: 1, offset: 0, limit: 151 },
+    { name: 2, offset: 151, limit: 100 },
+    { name: 3, offset: 251, limit: 135 },
+    { name: 4, offset: 386, limit: 107 },
+    { name: 5, offset: 493, limit: 156 },
+    { name: 6, offset: 649, limit: 72 },
+    { name: 7, offset: 721, limit: 88 },
+    { name: 8, offset: 809, limit: 96 },
+    { name: 9, offset: 905, limit: 120 },
+    { name: 0, offset: 1025, limit: 500 },
+]
