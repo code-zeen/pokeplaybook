504c2b6ec669a9615c9304a6c5038481bd74e3a7
김애진 <77660676+code-zeen@users.noreply.github.com>
2024-11-17 13:02:43 +0900

    add query config by generation

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,22 @@ function App() {
         }
     }, [ selectedPokemonId ])
 
+    const generations = [
+        { name: 1, offset: 0, limit: 151 },
+        { name: 2, offset: 151, limit: 100 },
+        { name: 3, offset: 251, limit: 135 },
+        { name: 4, offset: 386, limit: 107 },
+        { name: 5, offset: 493, limit: 156 },
+        { name: 6, offset: 649, limit: 72 },
+        { name: 7, offset: 721, limit: 88 },
+        { name: 8, offset: 809, limit: 96 },
+        { name: 9, offset: 905, limit: 120 },
+    ]
+
 
     const fetchAllPokemons = async () => {
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100')
+        const index = 8
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${generations[index].offset}&limit=${generations[index].limit}`)
         const data = await res.json()
 
         setPokemons(data.results.map((pokemon: { name: string, url: string }) => ({
