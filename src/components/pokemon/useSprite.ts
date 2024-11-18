8484ab8100f27f6ddfb22bceac064abed60c89f6
code-zeen <aejin.km@gmail.com>
2024-11-18 13:24:15 +0900

    create useSprite to lazily fetch sprite image

---
+++ src/components/pokemon/useSprite.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import { getSprite, SpriteEnum } from './getSprite'
+
+const useSprite = (spriteEnum: SpriteEnum, id: number) => {
+    const [spriteSrc, setSpriteSrc] = useState<string>("")
+
+    useEffect(() => {
+        const fetchSprite = async () => {
+            const sprite = await getSprite(spriteEnum, id)
+            setSpriteSrc(sprite || '')
+        }
+        fetchSprite()
+    }, [spriteEnum, id])
+
+    return { spriteSrc }
+}
+
+export default useSprite
