cc735b2ea77443a2c0844fbe4598bb237e88eb93
code-zeen <aejin.km@gmail.com>
2024-11-19 16:37:07 +0900

    add a temporary name to the MenuBar

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -17,7 +17,7 @@ function MenuBar({ handleKeyDown }: MenuBarProps) {
     return (
         <div className="top-0 left-0 w-full bg-gray-800 text-white shadow-md z-50">
             <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-2">
-                <div className="text-xl font-bold"></div>
+                <div className="text-xl font-bold">Bareruya</div>
 
                 <div className="flex-grow mx-8">
                     <Input
