3a83cef50f039b269ef0854ba002e5cb209ccd9b
code-zeen <aejin.km@gmail.com>
2024-11-22 16:06:47 +0900

    refactor baseUrl of fetch

--- src/features/pokemon-cards/fetch/fetchPokemon.ts
+++ src/features/pokemon-cards/fetch/fetchPokemon.ts
@@ -1,11 +1,13 @@
+const baseUrl = 'https://pokeapi.co/api/v2'
+
 export const fetchPokemonByNameOrId = async (nameOrId: string | number) => {
-    const resPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrId}`)
+    const resPokemon = await fetch(`${baseUrl}/pokemon/${nameOrId}`)
     const dataPokemon = await resPokemon.json()
 
-    const resAbility = await fetch(`https://pokeapi.co/api/v2/ability/${dataPokemon?.abilities[0].ability.name}`)
+    const resAbility = await fetch(`${baseUrl}/ability/${dataPokemon?.abilities[0].ability.name}`)
     const dataAbility = await resAbility.json()
 
-    const resMove = await fetch(`https://pokeapi.co/api/v2/move/${dataPokemon?.moves[0].move.name}`)
+    const resMove = await fetch(`${baseUrl}/move/${dataPokemon?.moves[0].move.name}`)
     const dataMove = await resMove.json()
 
     return { baseData: dataPokemon, ability: dataAbility, move: dataMove }
