9ce45f059de1fcb4f3eaba75674f3f7ebf9fb413
code-zeen <aejin.km@gmail.com>
2024-11-22 16:44:53 +0900

    move useSprite into hook/

--- src/features/pokedex/PokeballGrayBg.tsx
+++ src/features/pokedex/PokeballGrayBg.tsx
@@ -1,4 +1,4 @@
-import useSprite, { SpriteEnum } from '@/entities/pokemon/useSprite.ts'
+import useSprite, { SpriteEnum } from '@/entities/pokemon/hooks/useSprite.ts'
 
 interface PokeballGrayBgProps {
     id: number
