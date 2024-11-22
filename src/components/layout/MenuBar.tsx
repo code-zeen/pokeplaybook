6943089ac83b4a6d938c595a051ddb69724530d8
code-zeen <aejin.km@gmail.com>
2024-11-22 13:02:54 +0900

    rename project to PokePlaybook

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -14,7 +14,7 @@ function MenuBar() {
             <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-2">
                 <div className="flex items-center gap-2">
                     <SidebarTrigger />
-                    <h1 className="text-xl font-bold">Bareruya</h1>
+                    <h1 className="text-xl font-bold">PokePlaybook</h1>
                 </div>
 
                 <div>
