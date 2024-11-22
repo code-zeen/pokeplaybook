6b861fee0cd2ae279a46dcca3ffb41592a834bd2
code-zeen <aejin.km@gmail.com>
2024-11-22 13:37:35 +0900

    initialize RTK files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { store } from '@/store/store.ts'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router.tsx'
 
 function App() {
 
     return (
-        <RouterProvider router={router} />
+        <DndProvider backend={HTML5Backend}>
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
+        </DndProvider>
     )
 }
 
