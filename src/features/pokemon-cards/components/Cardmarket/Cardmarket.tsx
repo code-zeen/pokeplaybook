b37090f34ee880b86aee656832ae5dbf3739b89b
code-zeen <aejin.km@gmail.com>
2025-05-15 17:20:04 +0900

    add card average price

--- src/features/pokemon-cards/components/Cardmarket/Cardmarket.tsx
+++ src/features/pokemon-cards/components/Cardmarket/Cardmarket.tsx
@@ -15,10 +15,22 @@ export default function Cardmarket() {
     }
 
     return (
-        <div className="flex flex-wrap gap-1 px-1 py-2">
+        <div className="flex flex-wrap gap-1 px-1 py-2 max-w-7xl mx-auto">
             {pokemonCards?.map(card => (
-                <div key={card.id}
-                     className="border rounded-xl bg-white w-[calc(50%-0.125rem)] p-2">
+                <div
+                    key={card.id}
+                    className="relative flex flex-col border border-gray-100 rounded-md bg-gray-50  p-2
+                        w-[calc(50%-0.125rem)]
+                        sm:w-[calc(33.333%-0.167rem)]
+                        md:w-[calc(25%-0.1875rem)]
+                        lg:w-[calc(20%-0.2rem)]"
+                >
+                    {card.cardmarket && (
+                        <span
+                            className="absolute right-1 top-1 bg-red-600 border border-white rounded px-1 text-sm md:text-sm text-white">
+                            {card.cardmarket.prices.averageSellPrice.toString().replace('.', ',')}€
+                        </span>
+                    )}
                     <img src={card.images.small} alt={card.name}
                          className="object-contain" />
                     <div className="flex flex-col items-center">
