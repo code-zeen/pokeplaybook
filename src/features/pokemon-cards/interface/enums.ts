53bb479e2cd7d6adcee1e5dba26c01fe027e2a06
code-zeen <77660676+code-zeen@users.noreply.github.com>
2025-06-03 14:27:33 +0900

    add 3 enums that are used as search criteria

--- src/features/pokemon-cards/interface/enums.ts
+++ src/features/pokemon-cards/interface/enums.ts
@@ -11,3 +11,63 @@ export enum PokemonCardTypeEnum {
     STEEL = 'Metal',
     WATER = 'Water'
 }
+
+export enum PokemonCardSubTypeEnum {
+    BREAK = 'BREAK',
+    BABY = 'Baby',
+    BASIC = 'Basic',
+    EX = 'EX',
+    GX = 'GX',
+    GOLDENROD_GAME_CORNER = 'Goldenrod Game Corner',
+    ITEM = 'Item',
+    LEGEND = 'LEGEND',
+    LEVEL_UP = 'Level-Up',
+    MEGA = 'MEGA',
+    POKEMON_TOOL = 'Pokémon Tool',
+    POKEMON_TOOL_F = 'Pokémon Tool F',
+    RAPID_STRIKE = 'Rapid Strike',
+    RESTORED = 'Restored',
+    ROCKETS_SECRET_MACHINE = 'Rocket\'s Secret Machine',
+    SINGLE_STRIKE = 'Single Strike',
+    SPECIAL = 'Special',
+    STADIUM = 'Stadium',
+    STAGE_ONE = 'Stage 1',
+    STAGE_TWO = 'Stage 2',
+    SUPPORTER = 'Supporter',
+    TAG_TEAM = 'TAG TEAM',
+    TECHNICAL_MACHINE = 'Technical Machine',
+    V = 'V',
+    VMAX = 'VMAX'
+}
+
+export enum PokemonCardSuperTypeEnum {
+    ENERGY = 'Energy',
+    POKEMON = 'Pokémon',
+    TRAINER = 'Trainer'
+}
+
+export enum PokemonCardRarityEnum {
+    AMAZING_RARE = 'Amazing Rare',
+    COMMON = 'Common',
+    LEGEND = 'LEGEND',
+    PROMO = 'Promo',
+    RARE = 'Rare',
+    RARE_ACE = 'Rare ACE',
+    RARE_BREAK = 'Rare BREAK',
+    RARE_HOLO = 'Rare Holo',
+    RARE_HOLO_EX = 'Rare Holo EX',
+    RARE_HOLO_GX = 'Rare Holo GX',
+    RARE_HOLO_LV_X = 'Rare Holo LV.X',
+    RARE_HOLO_STAR = 'Rare Holo Star',
+    RARE_HOLO_V = 'Rare Holo V',
+    RARE_HOLO_VMAX = 'Rare Holo VMAX',
+    RARE_PRIME = 'Rare Prime',
+    RARE_PRISM_STAR = 'Rare Prism Star',
+    RARE_RAINBOW = 'Rare Rainbow',
+    RARE_SECRET = 'Rare Secret',
+    RARE_SHINING = 'Rare Shining',
+    RARE_SHINY = 'Rare Shiny',
+    RARE_SHINY_GX = 'Rare Shiny GX',
+    RARE_ULTRA = 'Rare Ultra',
+    UNCOMMON = 'Uncommon'
+}
